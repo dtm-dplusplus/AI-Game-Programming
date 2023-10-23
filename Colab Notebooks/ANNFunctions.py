bd6ee72e1e14e990dfd79d1b7a7cf1260315308d
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2023-10-23 16:51:30 +0100

    Colab Basic Sync Support

--- Colab Notebooks/ANNFunctions.py
+++ Colab Notebooks/ANNFunctions.py
@@ -31,5 +31,16 @@ def Learn(W, LEARN_RATE, X, E):
 
 
 # LOGGING #
-def PrintWeights(W1,W2):
-    print("w1 = " + str(W1) + " w2 = " + str(W2))
+def PrintEpochs(EPOCHS_COUNT):
+    print("Epoch Units = " + str(EPOCHS_COUNT))
+
+def PrintTest(test):
+    print(test)
+
+def PrintWeights(WEIGHT):
+    i = 0
+    while(i < len(WEIGHT)):
+        print("w" + str(i) + " = " + str(WEIGHT[i]))
+        i += 1
+
+
