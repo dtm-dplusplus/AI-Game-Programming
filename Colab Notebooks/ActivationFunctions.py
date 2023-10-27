8345e666e800832c375e62bb530e2ac388b5030a
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2023-10-27 16:36:58 +0100

    Back Propagation Algorithm
    Implemented a Back Propagation Algorithm. It's currently a work in progress.

--- Colab Notebooks/ActivationFunctions.py
+++ Colab Notebooks/ActivationFunctions.py
@@ -2,47 +2,24 @@ import numpy as np
 
 # ACTIVATION FUNCTIONS #
 # Sign Function
-def Sign(X, THRESHOLD): 
-    if X >= THRESHOLD: return 1
-    else: return -1
+def Sign(X, THRESHOLD = 0.0): 
+    if X >= THRESHOLD: return 1.0
+    else: return -1.0
 
 # Step function
-def Step(X, THRESHOLD): 
-    if X >= THRESHOLD: return 1
-    else: return 0
+def Step(X, THRESHOLD = 0.0): 
+    if X >= THRESHOLD: return 1.0
+    else: return 0.0
 
 # Sigmoid function
-def Sigmoid(X, THRESHOLD): 
-   return (1 / (1 + np.exp(-(X- THRESHOLD))))
+def Sigmoid(X): 
+   return 1.0 / (1.0 + np.exp(-X))
 
-def Linear(X, THRESHOLD):
+# Sigmoid Derivative function
+def SigmoidDelta(Y): 
+   return Y * (1-Y)
+
+def Linear(X, THRESHOLD = 0.0):
     return X - THRESHOLD
-
-# EPOCH FUNCTIONS #
-# Input Function
-def Input(X1,W1, X2,W2):
-    return ((X1*W1) + (X2*W2))
-
-# Error Function
-def Error(YD, Y):
-    return YD - Y
-
-# Input Function
-def Learn(W, LEARN_RATE, X, E):
-    return (W + (LEARN_RATE * X * E))
-
-
-# LOGGING #
-def PrintEpochs(EPOCHS_COUNT):
-    print("Epoch Units = " + str(EPOCHS_COUNT))
-
-def PrintTest(test):
-    print(test)
-
-def PrintWeights(WEIGHT):
-    i = 0
-    while(i < len(WEIGHT)):
-        print("w" + str(i) + " = " + str(WEIGHT[i]))
-        i += 1
 
 
